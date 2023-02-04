60e7c94d1a24338bdeab1f2622cf30ffb15c438d
Jim Moua <jim_dm@hotmail.com>
2023-02-03 21:02:08 -0800

    deleted test scripts and default titles/logos

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Banner from 'react-js-banner';
+import Exhibit from 'react-exhibit';
 
 function App() {
   return (
-    <div className="App">
+    <Banner
+      title="Sextant"
+    />
+  );
+}
+
+export default App;
+
+/*
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +30,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+*/
